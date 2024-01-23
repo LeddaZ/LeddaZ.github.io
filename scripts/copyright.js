ce00b5dfd161c7e4dce331ee0712f5bcf425877b
Leonardo Ledda <leonardoledda@gmail.com>
2024-01-23 15:05:53 +0100

    treewide: Set copyright year dynamically
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ scripts/copyright.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    let today = new Date();
+    let year = today.getFullYear();
+
+    $("#footer").html($("#footer").html() + year + " Leonardo Ledda (jk lmao)");
+
+})
